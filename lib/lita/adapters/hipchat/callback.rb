88a45bcf0c50c94f1fde2630ee32069e28938ca5
Jimmy Cuadra <jimmy@jimmycuadra.com>
2013-06-23 04:26:47 -0700

    Implement Callback#muc_message and user lookup methods.

--- lib/lita/adapters/hipchat/callback.rb
+++ lib/lita/adapters/hipchat/callback.rb
@@ -19,6 +19,32 @@ module Lita
             robot.receive(message)
           end
         end
+
+        def muc_message(muc)
+          muc.on_message do |time, nick, text|
+            user = user_by_name(nick)
+            source = Source.new(user, muc.jid.bare.to_s)
+            message = Message.new(robot, text, source)
+            robot.receive(message)
+          end
+        end
+
+        private
+
+        def user_by_jid(jid)
+          user_data = roster[jid].attributes
+
+          User.create(
+            user_data["jid"],
+            name: user_data["name"],
+            mention_name: user_data["mention_name"]
+          )
+        end
+
+        def user_by_name(name)
+          jid = roster.items.detect { |jid, item| item.iname == name }.first
+          user_by_jid(jid)
+        end
       end
     end
   end
--- spec/lita/adapters/hipchat/callback_spec.rb
+++ spec/lita/adapters/hipchat/callback_spec.rb
@@ -4,7 +4,28 @@ describe Lita::Adapters::HipChat::Callback do
   subject { described_class.new(robot, roster) }
 
   let(:robot) { double("Lita::Robot") }
-  let(:roster) { double("Jabber::Roster::Helper") }
+  let(:roster_item) { double("Jabber::Roster::RosterItem", iname: "Carl") }
+  let(:roster) do
+    double("Jabber::Roster::Helper", items: { "user_id" => roster_item })
+  end
+  let(:user) { double("Lita::User") }
+  let(:source) { double("Lita::Source") }
+  let(:message) { double("Lita::Message") }
+
+  before do
+    allow(roster).to receive(:[]).with("user_id").and_return(
+      double("RosterItem", attributes: {
+        "jid" => "user_id",
+        "name" => "Carl",
+        "mention_name" => "@Carl"
+      })
+    )
+    allow(Lita::User).to receive(:create).with(
+      "user_id",
+      name: "Carl",
+      mention_name: "@Carl"
+    ).and_return(user)
+  end
 
   it "has a robot" do
     expect(subject.robot).to eq(robot)
@@ -16,19 +37,15 @@ describe Lita::Adapters::HipChat::Callback do
 
   describe "#private_message" do
     let(:client) { double("Jabber::Client") }
-    let(:message) { double("Lita::Message") }
     let(:jabber_message) do
-      double("Jabber::Message", type: :chat, from: "jid", body: "foo")
+      double("Jabber::Message", type: :chat, from: "user_id", body: "foo")
     end
-    let(:source) { double("Lita::Source") }
-    let(:user) { double("Lita::User") }
 
     before do
       allow(client).to receive(:add_message_callback).and_yield(jabber_message)
     end
 
     it "sends the message to the robot with the proper source and body" do
-      allow(subject).to receive(:user_by_jid).with("jid").and_return(user)
       allow(Lita::Source).to receive(:new).with(user).and_return(source)
       allow(Lita::Message).to receive(:new).with(
         robot,
@@ -44,6 +61,29 @@ describe Lita::Adapters::HipChat::Callback do
       allow(jabber_message).to receive(:type).and_return(:error)
       expect(robot).not_to receive(:receive)
       subject.private_message(client)
+    end
+  end
+
+  describe "#muc_message" do
+    let(:jid) { double("Jabber::JID", bare: "room_id") }
+    let(:muc) { double("Jabber::MUC::SimpleMUCClient", jid: jid) }
+
+    before do
+      allow(muc).to receive(:on_message).and_yield(nil, "Carl", "foo")
+    end
+
+    it "sends the message to the robot with the proper source and body" do
+      allow(Lita::Source).to receive(:new).with(
+        user,
+        "room_id"
+      ).and_return(source)
+      allow(Lita::Message).to receive(:new).with(
+        robot,
+        "foo",
+        source
+      ).and_return(message)
+      expect(robot).to receive(:receive).with(message)
+      subject.muc_message(muc)
     end
   end
 end
