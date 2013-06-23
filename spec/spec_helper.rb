04f31a54eb21e3a2d0d3af9dc0655c768fb10a77
Jimmy Cuadra <jimmy@jimmycuadra.com>
2013-06-23 05:09:07 -0700

    Include Lita::RSpec to for the Redis test isolation.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,8 @@ require "lita-hipchat"
 require "lita/rspec"
 
 RSpec.configure do |config|
+  config.include Lita::RSpec
+
   config.before do
     allow(Lita).to receive(:logger).and_return(double("Logger").as_null_object)
   end
